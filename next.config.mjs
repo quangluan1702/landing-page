89f080a1a9082b3d9b9496825d1bfc0ce1094295
quangluan <luan.ha@stunited.vn>
2024-11-07 17:24:54 +0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // output: 'export',
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'http',
+          hostname: '127.0.0.1', // Only the IP address here
+          port: '1337',          // Specify the port separately
+          pathname: '/uploads/**', // Path without leading or trailing slashes
+        },
+      ],
+    },
+};
 
 export default nextConfig;
