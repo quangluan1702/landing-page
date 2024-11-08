8317b807c4c728231dd9d9d0974753a4868543c2
quangluan <luan.ha@stunited.vn>
2024-11-08 09:44:22 +0700

    sdf

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import Image from "next/image";
+import dynamic from "next/dynamic";
+
 
 interface IBackgroundImage {
   backgroundImage: any;
@@ -6,35 +7,34 @@ interface IBackgroundImage {
   subtitle: string;
   description: string;
   IsPublic: boolean;
-}
+};
+
+const DynamicImageComponent = dynamic(() => import("next/image"), { ssr: false });
 
 export default async function Home() {
-  const request = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/landing-activities?populate=*`,
-    {
-      // headers: {
-      //   authorization: `Bearer ${process.env.API_TOKEN}`
-      // }
-    }
-  );
-  const res = await request.json();
-  const backgroundImages: IBackgroundImage[] = res.data;
+  let backgroundImages: IBackgroundImage[] = [];
+
+  try {
+    const request = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/landing-activities?populate=*`);
+    const res = await request.json();
+    backgroundImages = res.data || [];
+  } catch (error) {
+    console.error("Error fetching background images:", error);
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       {backgroundImages
         .filter(({ IsPublic }) => IsPublic)
-        .map(
-          ({ backgroundImage, title, subtitle }) =>
-            typeof window !== "undefined" && (
-              <Image
-                src={`${process.env.NEXT_PUBLIC_BASE_URL}${backgroundImage?.formats?.thumbnail?.url}`}
-                key={subtitle}
-                alt={title}
-                width={1000}
-                height={1000}
-              />
-            )
-        )}
+        .map(({ backgroundImage, title, subtitle }) => (
+          <DynamicImageComponent
+            src={`${process.env.NEXT_PUBLIC_BASE_URL}${backgroundImage?.formats?.thumbnail?.url}`}
+            key={subtitle}
+            alt={title}
+            width={1000}
+            height={1000}
+          />
+        ))}
     </div>
   );
 }
