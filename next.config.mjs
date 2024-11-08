749318f05d66ac7b4ee14fa4da23d7f6ef67d28d
quangluan <luan.ha@stunited.vn>
2024-11-08 11:33:48 +0700

    sdf

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,13 @@ const nextConfig = {
   // output: "export",
 
   images: {
-    domains: ['4-create-langding-api.preview.cms.devplus.edu.vn'],
+    
     remotePatterns: [
       {
         protocol: "https",
         hostname: "4-create-langding-api.preview.cms.devplus.edu.vn", // Add this hostname
         pathname: "/uploads/**", // Specify the path pattern
+        domains: '4-create-langding-api.preview.cms.devplus.edu.vn',
       }
     ],
   },
