a3d727d130721eab227068c0f1f50f67701d5829
quangluan <luan.ha@stunited.vn>
2024-11-08 15:10:03 +0700

    sd111aaaaaa22aaaaf

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-
   images: {
-    
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-        // port: '',
-        // pathname: '/uploads/**',  // Specify the path if necessary
-      }
-    ],
+    domains: ['4-create-langding-api.preview.cms.devplus.edu.vn'],
   },
+  // images: {
+    
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "**",
+  //       // port: '',
+  //       // pathname: '/uploads/**',  // Specify the path if necessary
+  //     }
+  //   ],
+  // },
 };
 
 export default nextConfig;
