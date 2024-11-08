eae63af63b1f59339b13286f52d8263fc5c671a7
quangluan <luan.ha@stunited.vn>
2024-11-08 09:38:56 +0700

    adf

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,40 @@
 import Image from "next/image";
 
-export const runtime = "edge";
-
 interface IBackgroundImage {
-  backgroundImage:any
+  backgroundImage: any;
   title: string;
   subtitle: string;
   description: string;
-  IsPublic: boolean
-};
+  IsPublic: boolean;
+}
 
-export default async function Home  () {
-  const request = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/landing-activities?populate=*`, {
-    // headers: {
-    //   authorization: `Bearer ${process.env.API_TOKEN}`
-    // }
-  });  
-  const res=  await request.json()
-  const backgroundImages:IBackgroundImage[] = res.data
+export default async function Home() {
+  const request = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/landing-activities?populate=*`,
+    {
+      // headers: {
+      //   authorization: `Bearer ${process.env.API_TOKEN}`
+      // }
+    }
+  );
+  const res = await request.json();
+  const backgroundImages: IBackgroundImage[] = res.data;
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      
-        {backgroundImages
+      {backgroundImages
         .filter(({ IsPublic }) => IsPublic)
-        .map(({ backgroundImage, title, subtitle }) => (
-          <Image src={`${process.env.NEXT_PUBLIC_BASE_URL}${backgroundImage?.formats?.thumbnail?.url}`} key={subtitle} alt={title}  width={1000} height={1000}></Image>
-
-        ))}
+        .map(
+          ({ backgroundImage, title, subtitle }) =>
+            typeof window !== "undefined" && (
+              <Image
+                src={`${process.env.NEXT_PUBLIC_BASE_URL}${backgroundImage?.formats?.thumbnail?.url}`}
+                key={subtitle}
+                alt={title}
+                width={1000}
+                height={1000}
+              />
+            )
+        )}
     </div>
   );
 }
